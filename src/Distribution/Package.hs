db3bbfb9f6e4fc64165c964bc12cb683e332ca7b
Piotr Bogdan <ppbogdan@gmail.com>
2017-07-21 17:04:42 +0100

    Remove left over example JSON.

--- src/Distribution/Package.hs
+++ src/Distribution/Package.hs
@@ -5,41 +5,7 @@ import           Protolude
 import           Data.Aeson
 import Data.Char
 import qualified Data.Text as Text
-
-{-
-"nixos.paperkey" : {
-      "system" : "x86_64-linux",
-      "name" : "paperkey-1.3",
-      "meta" : {
-         "platforms" : [
-            "i686-linux",
-            "x86_64-linux",
-            "armv5tel-linux",
-            "armv6l-linux",
-            "armv7l-linux",
-            "aarch64-linux",
-            "mips64el-linux"
-         ],
-         "maintainers" : [
-            "Sven Keidel <svenkeidel@gmail.com>"
-         ],
-         "description" : "Store OpenPGP or GnuPG on paper",
-         "license" : {
-            "shortName" : "gpl2",
-            "fullName" : "GNU General Public License v2.0 only",
-            "url" : "http://spdx.org/licenses/GPL-2.0",
-            "spdxId" : "GPL-2.0"
-         },
-         "position" : "/home/pbogdan/nixpkgs/pkgs/tools/security/paperkey/default.nix:16",
-         "outputsToInstall" : [
-            "out"
-         ],
-         "homepage" : "http://www.jabberwocky.com/software/paperkey/",
-         "longDescription" : "A reasonable way to achieve a long term backup of OpenPGP (GnuPG, PGP, etc)\nkeys is to print them out on paper. Paper and ink have amazingly long\nretention qualities - far longer than the magnetic or optical means that\nare generally used to back up computer data.\n"
-      }
-   },
-
--}
+import           Data.Vector (Vector)
 
 data Package = Package
   { packageSystem :: Text
