f66b0a3bbc3dee6d7a626fc970dceb18bfc875b7
Piotr Bogdan <ppbogdan@gmail.com>
2017-07-22 15:06:45 +0100

    Add an introduction text on the report.

--- src/Nix/Cve.hs
+++ src/Nix/Cve.hs
@@ -41,7 +41,12 @@ report cvePath pkgsPath mtsPath = do
             "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
         ]
     body_ $
-      container_ $
+      container_ $ do
+      h1_ "Introduction"
+      p_ "This is POC that performs a scan against JSON feed from National Vulnerability Database https://nvd.nist.gov/vuln/data-feeds#JSON_FEED and recent-ish checkout of release-17.03 branch of nixpkgs."
+      p_ "As this is an early prototype it may not be completely accurate. It also doesn't scan all of packages such as those nested deeper in the attribute hierarchy of nixpkgs. Not all of CVEs may be relevant as well so addressing them would require further manual review - for example if a security patch has been applied without corresponding version bump of the package."
+      p_ "When listing maintainers the assumption that their handle is a valid GitHub username may not hold."
+      h2_ "Potential CVEs"
       table_ [class_ "table"] $ do
         thead_ $
           tr_ $ do
