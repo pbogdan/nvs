78659dc3b100b80101246b149dd80be82ddd2c88
Piotr Bogdan <ppbogdan@gmail.com>
2017-09-17 18:45:59 +0100

    Update shell-cmd.

--- default.nix
+++ default.nix
@@ -1,8 +1,8 @@
 { nixpkgs ? import <nixpkgs> {}, compiler ? "ghc802" }:
 let shell-cmd-src = (nixpkgs.fetchgit {
   url = "https://github.com/pbogdan/shell-cmd";
-  rev = "004e4313c7fca8dbd4dce0993093b4b8d0c56560";
+  rev = "d6c8435286edaa451a34afe1d5a7940a377e643f";
 });
 in nixpkgs.pkgs.haskell.packages.${compiler}.callPackage ./nvs.nix {
-  shell-cmd = import (shell-cmd-src) { };
+  shell-cmd = import (shell-cmd-src) { inherit nixpkgs compiler; };
 }
