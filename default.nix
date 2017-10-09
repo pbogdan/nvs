61e6e8f007f30e929c0d015b76c3363be41f8831
Piotr Bogdan <ppbogdan@gmail.com>
2017-10-09 15:11:12 +0100

    Add hash for shell-cmd.

--- default.nix
+++ default.nix
@@ -2,6 +2,7 @@
 let shell-cmd-src = (nixpkgs.fetchgit {
   url = "https://github.com/pbogdan/shell-cmd";
   rev = "d6c8435286edaa451a34afe1d5a7940a377e643f";
+  sha256 = "1y1hid7zs8kkxszgsqdd577fxfa0z812b1i3lbyn5lrnsdzg8kc9";
 });
 in nixpkgs.pkgs.haskell.packages.${compiler}.callPackage ./nvs.nix {
   shell-cmd = import (shell-cmd-src) { inherit nixpkgs compiler; };
