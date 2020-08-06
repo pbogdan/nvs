6567f28d9f2670ed11a4ccbb6b0114c0a6346cab
Piotr Bogdan <ppbogdan@gmail.com>
2020-08-06 15:16:34 +0100

    nix: clean up and refactor
    - make it possible to override the input `pkgs`
    - just use the default compiler, always
    - pin `ede` directly from upstream, instead of niv on my fork

--- default.nix
+++ default.nix
@@ -1,44 +1,39 @@
-{ sources ? (import ./nix/sources.nix)
-, compiler ? "ghc883"
+let
+  sources = import ./nix/sources.nix;
+in
+{ pkgs ? import sources.unstable { }
 , profiling ? false
 }:
 let
-  overlay = self: super: {
-    haskell = super.haskell // {
-      packages = super.haskell.packages // {
-        "${compiler}" = super.haskell.packages.${compiler}.override {
-          overrides = hself: hsuper: with self.haskell.lib; {
-            ede = overrideCabal hsuper.ede (
-              drv: {
-                src = sources."ede-trifecta-2.1";
-              }
-            );
-            repology-versions = import sources.repology-versions {
-              inherit sources;
-            };
+  hspkgs = pkgs.haskellPackages.override {
+    overrides = hself: hsuper: with pkgs.haskell.lib; {
+      ede = overrideCabal hsuper.ede (
+        drv: {
+          src = pkgs.fetchFromGitHub {
+            owner = "brendanhay";
+            repo = "ede";
+            rev = "5e0373b8a8c83ff2078a938795e30ec8038d228c";
+            sha256 = "1lb0q289p6lrc65adlacdx8xy8hrvcywbf6np7rilqdvvnyvlbgs";
           };
-        };
+        }
+      );
+
+      repology-versions = import sources.repology-versions {
+        inherit sources;
       };
     };
   };
 
-  pkgs = (
-    import sources.unstable {
-      overlays = [
-        overlay
-      ];
-    }
-  );
-
-  inherit (pkgs.haskellPackages)
+  inherit (hspkgs)
     callCabal2nix
-    shellFor
     ;
+
   inherit (pkgs.haskell.lib)
     enableLibraryProfiling
     enableExecutableProfiling
     justStaticExecutables
     ;
+
   inherit (pkgs)
     lib
     nix-gitignore
@@ -51,8 +46,7 @@ let
   ];
   nvs = callCabal2nix
     "nvs"
-    (nix-gitignore.gitignoreSource extra-source-excludes ./.)
-    {};
+    (nix-gitignore.gitignoreSource extra-source-excludes ./.) { };
 in
 if profiling then
   enableExecutableProfiling (enableLibraryProfiling nvs)
